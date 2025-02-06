d40fc7f32ec6d543337fe475bd351ab4db42f99c
IvanFrison <ivanfrisonivan@gmail.com>
2025-02-06 14:58:55 +0100

    Mejoras de aspecto

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import yt_dlp
 import os
 import tkinter as tk
 from tkinter import messagebox
-from PyQt5.QtWidgets import QApplication, QFileDialog
+from tkinter import filedialog
 
 def download_audio(link, path):
     try:
@@ -12,6 +12,7 @@ def download_audio(link, path):
         messagebox.showerror("Error", "Ha ocurrido un error al descargar el audio.")
         return 1
     messagebox.showinfo("Info", f"El audio se ha descargado correctamente.")
+    return 0
     
 
 def start_download():
@@ -21,32 +22,42 @@ def start_download():
         messagebox.showerror("Error", "Por favor, elige una carpeta de destino antes de comenzar la descarga.")
         return 1
     if(link):
-        download_audio(link, path)
-        return 0
+        if download_audio(link, path) == 0:
+            entry.delete(0, tk.END)
+            return 0
     else:
         messagebox.showerror("Error", "Por favor, introduce un enlace de YouTube válido.")
         return 2
         
 def choose_directory():
-    app = QApplication([])  # Crear la aplicación Qt
-    directory = QFileDialog.getExistingDirectory(None, "Elegir carpeta", "")  # Abrir el selector de carpetas
+    directory = filedialog.askdirectory()
     if directory:
         entry_path.delete(0, tk.END)
         entry_path.insert(0, directory)
         
 if __name__ == "__main__":
     root = tk.Tk()
+    width = 480
+    height = 200
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    root.geometry(f"{width}x{height}+{x}+{y}")
     root.title("YouTube Audio Downloader")
+    
+    frame = tk.Frame(root)
+    frame.pack(expand=True)
 
-    label = tk.Label(root, text="Introduce el enlace de YouTube")
-    entry = tk.Entry(root, width=50)
-    button_path = tk.Button(root, text="Elegir carpeta", command=choose_directory)
-    button = tk.Button(root, text="Descargar", command=start_download)
-    entry_path = tk.Entry(root, width=50)
+    label = tk.Label(frame, text="Introduce el enlace de YouTube", font=("Arial", 14))
+    entry = tk.Entry(frame, width=50, font=("Arial", 12))
+    button_path = tk.Button(frame, text="Elegir carpeta", command=choose_directory, width=15, height=2)
+    button = tk.Button(frame, text="Descargar", command=start_download, width=15, height=2)
+    entry_path = tk.Entry(frame, width=50)
 
-    label.pack()
-    entry.pack()
-    button_path.pack(pady=5)
-    button.pack()
+    label.grid(row=0, column=0, columnspan=2, pady=5)
+    entry.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+    button_path.grid(row=2, column=0, padx=5, pady=10, sticky="e")
+    button.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
     root.mainloop()
